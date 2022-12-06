a0ca870b86c2b8f0c31afe7b371ae73f3cf21c74
danielkhalils <dkses.hef@uea.edu.br>
2022-12-06 10:25:59 -0400

    Incializando o projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import { doc, setDoc, collection, addDoc, getDoc, getDocs } from 'firebase/firestore'
+import { db } from './firebaseConnection'
+import { useState } from 'react'
+import './App.css'
 
 function App() {
+  const[titulo, setTitulo] = useState('');
+  const[autor, setAutor] = useState('');
+  const[posts, setPosts] = useState([]);
+  
+
+  async function handleAdd(){
+
+    // await setDoc(doc(db, "posts", "145678"), {    
+    // titulo: titulo,
+    // autor: autor,
+    // })
+    // .then(() => {
+    //   console.log('Dados registrados no banco!')
+    // }).catch((error) => {
+    //   console.log('GEROU ERRO' + error)
+    // })
+
+    //Adicionar Post
+    await addDoc(collection(db, "posts"), {
+      titulo: titulo,
+      autor: autor,
+    }).then(() => {
+      console.log('Cadastrado com sucesso')
+      setAutor('')
+      setTitulo('')
+    }).catch((error) => {
+      console.log('ERRO' + error)
+    })
+  } 
+
+  //Buscar Posts
+  async function buscarPosts(){
+    
+  //Buscar Post
+  //   // const postRef = doc(db, "posts", "12345")
+  //   // await getDoc(postRef)
+  //   // .then((snapshot) => {
+  //   //   setAutor(snapshot.data().autor)
+  //   //   setTitulo(snapshot.data().titulo)
+  //   // }).catch((error) => {
+  //   //   console.log('ERRO' + error)
+  //   // })
+  // }
+
+    const postsRef = collection(db, "posts")
+    await getDocs(postsRef)
+    .then((snapshot) => {
+      let lista = [];
+
+      snapshot.forEach((doc) => {
+        lista.push({
+          id: doc.id,
+          titulo: doc.data().titulo,
+          autor: doc.data().autor,
+        })
+      })
+
+      setPosts(lista)
+
+    })
+    .catch((error) => {
+      console.log("Erro ao buscar")
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>ReactJS + Firebase</h1>
+
+      <div className='container'>
+        <label>Título:</label>
+        <textarea 
+          type='text'  
+          placeholder='Digite um título'
+          value={titulo}
+          onChange={ (e) => setTitulo(e.target.value)}
+        />
+
+        <label>Autor:</label>
+        <textarea 
+          type='text' 
+          placeholder='Digite o nome do autor'
+          value={autor}
+          onChange={ (e) => setAutor(e.target.value)}
+
+        />
+
+        <button onClick={handleAdd}>Cadastrar</button>
+        <button onClick={buscarPosts}>Buscar Posts</button>
+
+        <ul>
+          {posts.map( (post) => {
+            return(
+              <li key={post.id}>
+                <span>Título: {post.titulo} </span> <br/>
+                <span>Autor: {post.autor} </span> <br/>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App; 
---
+++ src/firebaseConnection.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAvWJUh4-bmEF5tzebtVh06PC1ybBwFqnU",
+    authDomain: "curso-d2978.firebaseapp.com",
+    projectId: "curso-d2978",
+    storageBucket: "curso-d2978.appspot.com",
+    messagingSenderId: "751813523858",
+    appId: "1:751813523858:web:abb9572a075096d304e94b",
+    measurementId: "G-CXQYRBHF4P"
+  };
+
+  const firebaseApp = initializeApp(firebaseConfig);
+
+  const db  = getFirestore(firebaseApp);
+
+  export { db };
